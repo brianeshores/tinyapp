5e491167ba24c54dba2f3cffa8d35bcf829c781c
Brian Murray <brianmurraysound@gmail.com>
2019-05-07 20:11:51 +0000

    added body parser and finished a function to generate a random string of 6 characters

--- express_server.js
+++ express_server.js
@@ -1,7 +1,18 @@
 var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
+var randomString = "";
 
+function generateRandomString() {
+  var charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*';
+  for (var i = 0; i < 6; i++) {
+  var randomPos = Math.floor(Math.random() * charSet.length);
+  randomString += charSet.substring(randomPos, randomPos+1);
+  }
+  return randomString;
+}
+generateRandomString();
+console.log("randomString: ", randomString);
 app.set("view engine", "ejs");
 
 var urlDatabase = {
@@ -9,6 +20,12 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -26,7 +43,10 @@ app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
-
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
